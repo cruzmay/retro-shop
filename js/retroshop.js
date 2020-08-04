ab8d549640fbcc52e9df1fdcd1889c6561993692
Pedro Cruz <pcruzmay@yahoo.com>
2020-08-03 20:29:19 -0500

    ajustes a responsive

--- js/retroshop.js
+++ js/retroshop.js
@@ -1,20 +1,50 @@
-const modal = document.getElementById('modal')
-const overlay = document.getElementById('overlay')
-const producto = document.getElementById('producto')
-const close = document.getElementById('close')
-const add = document.getElementById('add')
+function toogleMenu() {
+    // console.log('its working')
+    let menu = document.getElementById('hiddenMenu')
+    if (menu.style.left === "-400px") {
+        menu.style.left = "0"
+    }
+    else {
+        menu.style.left = "-400px"
 
-producto.addEventListener('click', (event) => {
-    console.log('tocame')
-    overlay.classList.toggle('off')
-    modal.classList.remove('modalout')
-    modal.classList.add('modalin')
-})
+    }
+}
+function openFilters() {
+   
+    let filters = document.getElementById('filters')
+    let expand = document.getElementById('expand')
 
-close.addEventListener('click', (event)=>{
-    modal.classList.remove('modalin')
-    modal.classList.add('modalout')
-    overlay.classList.toggle('off')
-    
-    
-})
+    if (filters.style.display === "none") {
+        filters.style.display = "flex"
+        expand.style.transform = "rotate(180deg)" 
+    }
+    else {
+        filters.style.display = "none"
+        expand.style.transform = "rotate(0deg)" 
+
+    }
+}
+window.addEventListener('resize', () => {
+    let ancho = window.outerWidth;
+    // console.log(ancho)
+    if(ancho >= 950) {
+        filters.style.display = "flex"
+    } else {
+        filters.style.display = "none"
+    }
+});
+
+
+function openModal() {
+    let modal = document.getElementById('modal')
+    let overlay = document.getElementById('overlay')
+    if(overlay.style.display === "none") {
+        overlay.style.display = 'flex'
+        modal.style.animation = 'modalIn 1s forwards'
+        
+    }
+    else {
+        modal.style.animation = 'modalOut 2s forwards'
+        overlay.style.display = 'none'
+    }
+}
